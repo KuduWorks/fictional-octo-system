e9eb84f08e9973bc318712cb74be8c60eef4bbce
Yves Laingui <105453134+kudu-star@users.noreply.github.com>
2025-10-13 23:19:53 +0300

    Refactor vnet.tf to use variables for address space and subnet prefix

--- terraform/vnet.tf
+++ terraform/vnet.tf
@@ -2,11 +2,11 @@ resource "azurerm_virtual_network" "main" {
   name                = "vnet-dev-01"
   location            = var.location
   resource_group_name = azurerm_resource_group.example.name
-  address_space       = ["10.0.0.0/16"]
+  address_space       = var.vnet_address_space
 
   subnet {
     name           = "subnet-dev-01"
-    address_prefix = "10.0.1.0/24"
+    address_prefix = var.subnet_prefix
   }
 
   tags = var.tags
