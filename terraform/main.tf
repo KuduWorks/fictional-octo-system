63e394d7e4336b2211cd957d8bac2b76c09cd499
Yves Laingui <105453134+kudu-star@users.noreply.github.com>
2025-10-13 22:42:20 +0300

    Add initial Terraform configuration files for Azure deployment

---
+++ terraform/main.tf
@@ -0,0 +1,19 @@
+terraform {
+    required_providers {
+        azurerm = {
+            source  = "hashicorp/azurerm"
+            version = "~> 3.0"
+        }
+    }
+    required_version = ">= 1.3.0"
+}
+
+provider "azurerm" {
+    features {}
+}
+
+resource "azurerm_resource_group" "example" {
+    name     = "rg-dev-01"
+    location = var.location
+    tags     = var.tags
+}
